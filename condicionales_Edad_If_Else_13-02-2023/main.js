2c84abf699208e30114e9ba7aafa2dc6261aa8b5
angierobayo9809 <angierobayo09@gmail.com>
2023-02-23 13:41:09 -0500

    first commit

---
+++ condicionales_Edad_If_Else_13-02-2023/main.js
@@ -0,0 +1,11 @@
+/* 1.	Haz un programa de nombre main.js y otro llamado index.html incluir el archivo
+ JavaScript en el <head></ head>, que has de grabar en una carpeta llamada condicionales_Edad_If_Else_FechaActual,
+  por medio del prompt pedir la edad y verificar si la edad ingresada es de 18 mostrar que “Es mayor de edad” 
+  del caso contrario mostrar un mensaje por medio de la consola o de la ventana emergente 
+  que diga “La edad ingresada no cumple con los parámetros “, usando el condicional If Else.*/
+
+let edad = prompt("Ingrese su edad");
+if (edad == 18 )
+    console.log("Es mayor de edad.");
+else
+    console.log("La edad ingresada no cumple con los parámetros.");
